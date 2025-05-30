6860018962175487f0de69f0e4bad7e349580027
ryoma-abe <liaozhenanbu387@gmail.com>
2025-05-31 07:30:24 +0900

    app._index.tsxにメタフィールド書き込みのアクション関数を追加し、WriteMeta.tsxから関連コードを削除。コードの整理と機能の統合を実施。

--- app/components/WriteMeta.tsx
+++ app/components/WriteMeta.tsx
@@ -1,50 +1,7 @@
-import { type ActionFunctionArgs, redirect } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
 import { Card } from "@shopify/polaris";
-import { authenticate } from "app/shopify.server";
 import { type loader } from "../routes/app._index";
 import { type Product } from "app/types/product";
-
-//メタフィールドに書き込み
-export async function action({ request }: ActionFunctionArgs) {
-  const { admin } = await authenticate.admin(request);
-  const formData = await request.formData();
-
-  const productId = formData.get("productId")?.toString();
-  const note = formData.get("note")?.toString();
-
-  if (!productId || !note) return redirect("/");
-
-  const mutation = `
-    mutation set($metafields: [MetafieldsSetInput!]!) {
-      metafieldsSet(metafields: $metafields) {
-        metafields {
-          id
-          value
-        }
-        userErrors {
-          field
-          message
-        }
-      }
-    }
-  `;
-
-  const variables = {
-    metafields: [
-      {
-        ownerId: productId,
-        namespace: "demo",
-        key: "note",
-        type: "single_line_text_field",
-        value: note,
-      },
-    ],
-  };
-
-  await admin.graphql(mutation, { variables });
-  return redirect("/app");
-}
 
 export default function WriteMeta() {
   const { products } = useLoaderData<typeof loader>();
