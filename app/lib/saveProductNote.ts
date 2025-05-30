c363a25e368eb9c3b5759d44bd813ce952b60dfe
ryoma-abe <liaozhenanbu387@gmail.com>
2025-05-31 08:25:53 +0900

    app._index.tsxのfetchProducts関数を使用して商品取得のロジックを改善し、コードの可読性を向上させるためにloader関数をリファクタリング。

---
+++ app/lib/saveProductNote.ts
@@ -0,0 +1,42 @@
+import { redirect, type ActionFunctionArgs } from "@remix-run/node";
+import { authenticate } from "app/shopify.server";
+
+export async function saveProductNote({ request }: ActionFunctionArgs) {
+  const { admin } = await authenticate.admin(request);
+  const formData = await request.formData();
+
+  const productId = formData.get("productId")?.toString();
+  const note = formData.get("note")?.toString();
+
+  if (!productId || !note) return redirect("/");
+
+  const mutation = `
+    mutation set($metafields: [MetafieldsSetInput!]!) {
+      metafieldsSet(metafields: $metafields) {
+        metafields {
+          id
+          value
+        }
+        userErrors {
+          field
+          message
+        }
+      }
+    }
+  `;
+
+  const variables = {
+    metafields: [
+      {
+        ownerId: productId,
+        namespace: "demo",
+        key: "note",
+        type: "single_line_text_field",
+        value: note,
+      },
+    ],
+  };
+
+  await admin.graphql(mutation, { variables });
+  return redirect("/app");
+}
